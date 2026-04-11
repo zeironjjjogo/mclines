3eabf704f864b6bcc19adf969d9e66849df4177b
zeironjjjogo <raraeabc@gmail.com>
2026-04-12 02:04:45 +0900

    Added README.md

--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/berhi/coding/mclines/test
-# Build directory: /home/berhi/coding/mclines/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mclines_test "/home/berhi/coding/mclines/test/build/mclines_test")
-set_tests_properties(mclines_test PROPERTIES  _BACKTRACE_TRIPLES "/home/berhi/coding/mclines/test/CMakeLists.txt;8;add_test;/home/berhi/coding/mclines/test/CMakeLists.txt;0;")
